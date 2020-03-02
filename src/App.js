9e132394ee9d38452941688e289760d7a6308a67
swdecker <swdecker@gmail.com>
2020-03-01 21:56:14 -0600

    added dynamic sort function and started sending sorted issues into the return

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 function App() {
   const [issues, setIssues] = useState([]);
+  const [sort, setSort] = useState("title");
   useEffect(()=>{
     fetch("https://api.github.com/repos/rails/rails/issues")
     .then(resp=>resp.json())
@@ -13,33 +14,77 @@ function App() {
       setIssues(issues)
     })
   },[])
+
+  // a function to make a simple sort for any property of an object
+  const propertySort= (property)=>{
+    return function(a,b){
+      if (a[property]>b[property]){return 1}
+      if (a[property]<b[property]){return -1}
+      return 0 
+    }
+  }
+
+  const issueSort = () =>{
+    let returnArray = [...issues]
+    switch (sort){
+      case "title":
+        returnArray.sort(propertySort('title'))
+        break
+      case "comments":
+        returnArray.sort((i1,i2)=>{
+          return i1.comments-i2.comments
+        })
+        break
+      case "created_at":
+        
+        break
+      case "updated_at":
+        
+        break
+      default:
+
+    }
+    return returnArray
+  }
+  const sortChangeHandler = (event)=>{
+    setSort(event.target.value)
+    console.log(sort)
+  }
   const issueDisplay= ()=>{
     console.log(issues[0])
-    return issues.map(issue=><Issue issue={issue} />)
+    return issueSort().map(issue=><Issue issue={issue} />)
   }
+  let sorts = ["title", "comments", "created_at", "updated_at"]
   return (
     <div className="App">
-      <div className="issues-list">
-      <table >
-        <thead>
-          <tr> 
-            <th colspan="5">
-              {"Rails Github issues"}
-            </th>
-          </tr>
-          <tr>
+      <h3>{"Sort or Filter Issues"}</h3>
+      <div className="sort">
+        <select onChange={sortChangeHandler} value={sort}>
+          {sorts.map(sortKey=><option key={sortKey} value={sortKey}>{sortKey}</option>)}
+        </select>
+      </div>
 
-            <th>Issue Title</th>
-            <th>Author</th>
-            <th>Description</th>
-            <th>Labels</th>
-            <th>Comment Count</th>
-          </tr>
-        </thead>
-        <tbody>
-          {issueDisplay()}
-        </tbody>
-      </table>
+      <div className="issues-list">
+        <table >
+          <thead>
+            <tr> 
+              <th colSpan="5">
+                {"Rails Github issues"}
+              </th>
+            </tr>
+            <tr>
+
+              <th>Issue Title</th>
+              <th>Author</th>
+              <th>Description</th>
+              <th>Labels</th>
+              <th>Comment Count</th>
+            </tr>
+          </thead>
+          <tbody>
+            {issueDisplay()}
+          </tbody>
+        </table>
       </div>
     </div>
   );
